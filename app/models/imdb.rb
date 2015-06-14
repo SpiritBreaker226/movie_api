616d0bd37eda3445ced946b0ff1536a08378a91d
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-14 13:57:57 -0400

    Add IMDB Token For Each API Call As It Is Required

--- app/models/imdb.rb
+++ app/models/imdb.rb
@@ -2,11 +2,15 @@ class Imdb
 	include HTTParty
 	base_uri 'myapifilms.com'
 
+	@@my_api_films_token = ENV["my_api_films"]
+
 	def self.find_by_title(options = {})
+		options[:token] = @@my_api_films_token
+
 		get("/imdb", query: options)
 	end
 
 	def self.find(imdb_id)
-		
+		options[:token] = @@my_api_films_token
 	end
 end
